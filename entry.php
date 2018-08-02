af93166102162e8489944e160495327038e1f58f
paragi <simon.riget@gmail.com>
2018-08-02 11:08:10 +0200

    CSP header changed

--- entry.php
+++ entry.php
@@ -41,6 +41,7 @@ $class=$alert?$alert."_alert":"";
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
+header("Content-Security-Policy: default-src 'self'");
 
 ?><!DOCTYPE HTML>
 <html>
